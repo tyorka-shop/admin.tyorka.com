d36bd0a206963ebe1e94536808907788259afb0b
s.ovchinnikov <s.ovchinnikov@unitedtraders.com>
2022-05-27 16:42:49 +0400

    fix: migration

--- packages/server/src/store/migrations/1.ts
+++ packages/server/src/store/migrations/1.ts
@@ -12,8 +12,8 @@ export const addDatesToProducts: Migrator = async (data, logger) => {
     const ctime = product.pictures.map(picId => {
       const picture = data.pictures.find(pic => pic.id === picId);
       const stat = picture ? fs.statSync(path.join(config.imagesFolder, picture.src)) : undefined
-      console.log(picture?.src, stat?.ctime)
-      return stat ? +stat?.ctime : 0
+      console.log(picture?.src, new Date(stat?.mtime || 0))
+      return stat ? +stat?.mtime : 0
     })
     .filter(Boolean)
     .sort((a, b) => (+a) - (+b))[0];
